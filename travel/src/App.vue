50f2fbd1a55c4524144e2a1b33d4ed1b26df5ebe
shawroc <coldplay1204@hotmail.com>
2019-02-21 20:03:33 +0800

    finish pages

--- travel/src/App.vue
+++ travel/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
@@ -11,11 +13,5 @@ export default {
 </script>
 
 <style>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c3e50;
-} */
+
 </style>
